9f1f10df8d3df4cd6c98e103e391098cf5bd1064
mal0101 <112941277+mal0101@users.noreply.github.com>
2025-10-17 11:25:10 +0100

    init

---
+++ src/test/java/com/main/coedit/CoEditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.coedit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoEditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
