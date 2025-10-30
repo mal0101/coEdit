6b1a23abeb3bb5f30a43db276b9a5b8237730bbc
mal0101 <112941277+mal0101@users.noreply.github.com>
2025-10-30 23:16:43 +0100

    implemented AuthController

--- src/main/java/com/main/editco/controller/AuthController.java
+++ src/main/java/com/main/editco/controller/AuthController.java
@@ -1,29 +1,34 @@
 package com.main.editco.controller;
 
+import com.main.editco.dao.entities.User;
+import com.main.editco.dto.LoginRequest;
+import com.main.editco.dto.RegisterRequest;
 import com.main.editco.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
     @Autowired AuthService authService;
     @PostMapping("/signup")
-    public String register(/*DTO*/) {
-        // TODO use authservice to register
-        return "User registered successfully";
+    public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest) {
+        User user = authService.register(registerRequest);
+        if (user == null) return  ResponseEntity.badRequest().body("email already exists");
+        return ResponseEntity.ok("User registered successfully");
     }
     @PostMapping("/login")
-    public String login(/*DTO*/) {
-        // TODO use authservice to login
-        return "JWT token";
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        String jwt = authService.login(loginRequest);
+        if (jwt == null) return  ResponseEntity.badRequest().body("Invalid Credentials");
+        return ResponseEntity.ok(jwt);
     }
     @GetMapping("/me")
-    public String currentUser(/*DTO*/) {
-        // TODO
-        return "Current user info";
+    public ResponseEntity<?> currentUser(Authentication authentication) {
+        String email = authentication.getName();
+        User user = authService.getCurrentUser(email);
+        return ResponseEntity.ok(user);
     }
 }
