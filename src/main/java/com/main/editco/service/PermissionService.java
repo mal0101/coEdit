120f37a704915d7fe15e0456365d36726ed73403
mal0101 <112941277+mal0101@users.noreply.github.com>
2025-10-31 11:38:29 +0100

    permission service implemented

--- src/main/java/com/main/editco/service/PermissionService.java
+++ src/main/java/com/main/editco/service/PermissionService.java
@@ -6,13 +6,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PermissionService {
     @Autowired PermissionRepository permissionRepository;
 
+    public Permission grantPermission(Permission permission) {
+        return permissionRepository.save(permission);
+    }
+
+    public boolean revokePermission(Long permissionId) {
+        if (permissionRepository.existsById(permissionId)) {
+            permissionRepository.deleteById(permissionId);
+            return true;
+        }
+        return false;
+    }
+
     public List<Permission> getPermissionsByDocumentId(Long documentId) {
         return permissionRepository.findByDocumentId(documentId);
     }
-    // TODO: add methods for granting/revoking permission
+
+    public List<Permission> getPermissionsByUserId(Long userId) {
+        return permissionRepository.findByUserId(userId);
+    }
+
+    public Optional<Permission> getPermission(Long id) {
+        return permissionRepository.findById(id);
+    }
 }
