aad8b721ab3cb6d9408453ba972686e46d2a1ff7
mal0101 <112941277+mal0101@users.noreply.github.com>
2025-11-07 22:22:48 +0100

    added JsonIgnoreProperties on relationships fields to break cycles

--- src/main/java/com/main/editco/dao/entities/Permission.java
+++ src/main/java/com/main/editco/dao/entities/Permission.java
@@ -1,5 +1,6 @@
 package com.main.editco.dao.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,10 +17,12 @@ public class Permission {
 
     @ManyToOne
     @JoinColumn(name = "document_id")
+    @JsonIgnoreProperties({"comments", "versions", "permissions", "owner"})
     private Document document;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @JsonIgnoreProperties({"passwordHashed", "ownedDocuments", "permissions", "comments"})
     private User user;
 
     @Enumerated(EnumType.STRING)
