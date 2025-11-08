1d7775c8fd77f549e928b1021d5f48aaa0a7627d
mal0101 <112941277+mal0101@users.noreply.github.com>
2025-11-08 18:38:43 +0100

    updated VersionHistoryController

--- src/main/java/com/main/editco/controller/VersionHistoryController.java
+++ src/main/java/com/main/editco/controller/VersionHistoryController.java
@@ -5,6 +5,7 @@ import com.main.editco.dao.entities.User;
 import com.main.editco.dao.entities.VersionHistory;
 import com.main.editco.dao.repositories.DocumentRepository;
 import com.main.editco.dao.repositories.UserRepository;
+import com.main.editco.service.PermissionCheckService;
 import com.main.editco.service.VersionHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,53 +27,79 @@ public class VersionHistoryController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PermissionCheckService  permissionCheckService;
+
     @PostMapping
-    public ResponseEntity<VersionHistory> addVersionHistory(@RequestBody VersionHistory versionHistory) {
+    public ResponseEntity<?> addVersionHistory(@RequestBody VersionHistory versionHistory, Authentication authentication) {
+        String email = authentication.getName();
+        User currentUser = userRepository.findByEmail(email);
+        Long documentId = versionHistory.getDocument().getId();
+
+        if (!permissionCheckService.canEdit(currentUser.getId(), documentId)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You don't have permission to create version history");
+        }
         VersionHistory createdVersionHistory = versionHistoryService.addVersionHistory(versionHistory);
-        return ResponseEntity.ok(createdVersionHistory);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdVersionHistory);
     }
 
     @GetMapping("/document/{documentId}")
-    public List<VersionHistory> getVersionHistory(@PathVariable Long documentId) {
-        return versionHistoryService.getVersionHistory(documentId);
+    public ResponseEntity<?> getVersionHistory(@PathVariable Long documentId, Authentication authentication) {
+        String email = authentication.getName();
+        User currentUser = userRepository.findByEmail(email);
+        if (!permissionCheckService.canView(currentUser.getId(), documentId)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You don't have permission to view this version history");
+        }
+        List<VersionHistory> versionHistory = versionHistoryService.getVersionHistory(documentId);
+        return ResponseEntity.status(HttpStatus.OK).body(versionHistory);
     }
 
     @GetMapping("/{versionId}")
-    public ResponseEntity<VersionHistory> getVersion(@PathVariable Long versionId) {
-        Optional<VersionHistory> version = versionHistoryService.getVersion(versionId);
-        return version.map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<?> getVersion(@PathVariable Long versionId,  Authentication authentication) {
+        String email = authentication.getName();
+        User currentUser = userRepository.findByEmail(email);
+        Optional<VersionHistory> versionOpt = versionHistoryService.getVersion(versionId);
+        if (!versionOpt.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        VersionHistory versionHistory = versionOpt.get();
+        Long documentId = versionHistory.getDocument().getId();
+        if (!permissionCheckService.canView(currentUser.getId(), documentId)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body("You don't have permission to view this version history");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(versionHistory);
     }
 
     @PostMapping("/{versionId}/restore")
     public ResponseEntity<?> restoreVersion(@PathVariable Long versionId, Authentication authentication) {
+        String email = authentication.getName();
+        User currentUser = userRepository.findByEmail(email);
         Optional<VersionHistory> versionOpt = versionHistoryService.getVersion(versionId);
         if (!versionOpt.isPresent()) {
             return ResponseEntity.notFound().build();
         }
         VersionHistory versionToRestore = versionOpt.get();
-        Optional<Document> docOpt = documentRepository.findById(versionToRestore.getId());
-        if (!docOpt.isPresent()) {
+        Long documentId = versionToRestore.getDocument().getId();
+
+        if (!permissionCheckService.canEdit(currentUser.getId(), documentId)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body("You don't have permission to restore this version history");
+        }
+        Optional<Document> documentOpt = documentRepository.findById(documentId);
+        if (!documentOpt.isPresent()) {
             return ResponseEntity.notFound().build();
         }
-        Document document =  docOpt.get();
-        // getting curr user whos doing the restore
-        String userEmail = authentication.getName();
-        User currentUser = userRepository.findByEmail(userEmail);
-        if  (currentUser == null) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("User not found");
-        }
+        Document document = documentOpt.get();
         VersionHistory currentStateBackup = new VersionHistory();
         currentStateBackup.setDocument(document);
         currentStateBackup.setContent(document.getContent());
         currentStateBackup.setEditedBy(currentUser);
         currentStateBackup.setTimestamp(versionToRestore.getTimestamp());
-
-        // save backup
         versionHistoryService.addVersionHistory(currentStateBackup);
         document.setContent(versionToRestore.getContent());
         document.setUpdatedAt(versionToRestore.getTimestamp());
         documentRepository.save(document);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.OK).body(document);
     }
 }
